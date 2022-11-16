6aa35f9b15100fcdecb589a0e840e3e260ad5a48
Xayshondet <Roma00952011@mail.ru>
2022-11-16 09:22:24 +0200

    init

---
+++ src/main/java/md/expres/Expres/ExpresApplication.java
@@ -0,0 +1,13 @@
+package md.expres.Expres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/expres/Expres/ExpresApplicationTests.java
@@ -0,0 +1,13 @@
+package md.expres.Expres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
